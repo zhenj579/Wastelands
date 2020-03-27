93edda59b10b0a4358d9cbffff53e16ec0f26cf3
Bexzod Abduvaliev <bexzodabduvaliev@gmail.com>
2020-03-27 15:53:56 -0400

    removed deleted import

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.Wastelands;
 import com.mygdx.game.Wastelanders;
 
 public class DesktopLauncher {
