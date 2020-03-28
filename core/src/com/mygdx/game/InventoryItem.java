f31d9412e8986918b03bf1b3db0de2a51019731b
Bexzod Abduvaliev <bexzodabduvaliev@gmail.com>
2020-03-28 16:59:26 -0400

    removed unnecessary interface implement

--- core/src/com/mygdx/game/InventoryItem.java
+++ core/src/com/mygdx/game/InventoryItem.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
-public abstract class InventoryItem implements Drawable{
+public abstract class InventoryItem {
 	
 	protected Texture itemTexture;
 	protected TextureRegion itemTextureRegion;
@@ -13,10 +13,6 @@ public abstract class InventoryItem implements Drawable{
 	public InventoryItem(String texturePath) {
 		itemTexture = new Texture(texturePath);
 		itemTextureRegion = new TextureRegion(itemTexture);
-	}
-	
-	public void Draw(Batch batch, float x, float y, float width, float heihgt) {
-		batch.draw(itemTextureRegion, x, y);
 	}
 	
 	public TextureRegion GetTextureRegion() {
