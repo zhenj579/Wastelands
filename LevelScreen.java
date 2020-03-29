ff55db13166fc36e6ea7b7f932f545ec743d7269
zhenj579 <50351080+zhenj579@users.noreply.github.com>
2020-03-29 16:18:24 -0400

    fixed a bug
    made it so that you must be within a certain range while clicking to pick up the trash

---
+++ LevelScreen.java
@@ -0,0 +1,51 @@
+package com.mygdx.game;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.math.Vector2;
+
+
+public class LevelScreen extends BaseScreen
+{
+    Map map;
+	private Player wastelander;
+	
+	
+    public void initialize() 
+    {   
+    	map = new Map("wasteland_test_map.tmx", mainStage);
+    	wastelander = new Player(590, 400, mainStage, uiTable);
+//    	Texture buttonTex = new Texture( Gdx.files.internal("rmcloseup.png") );
+//    	TextureRegion buttonRegion = new TextureRegion(buttonTex);
+//    	ButtonStyle buttonStyle = new ButtonStyle();
+//    	buttonStyle.up = new TextureRegionDrawable( buttonRegion );
+//    	Button test = new Button(buttonStyle);
+//    	uiTable.add(test);
+//    	uiTable.row();
+//    	Label playerName = new Label("John", BaseGame.labelStyle);
+//    	uiTable.add(playerName);
+//    	uiTable.row();
+//    	uiTable.add().padBottom(200);
+    	uiTable.setVisible(true);	
+    	
+    }
+    
+    public void update(float dt)
+    {
+    	Vector2 wastelanderCoordinates = new Vector2((int)wastelander.getX(), (int)wastelander.getY());
+    	WastelandTrash trash = map.getTrashAt(wastelanderCoordinates, wastelander);
+    	if(trash != null)
+    	{
+    		trash.destroy();
+//    		map.printObjectState();
+    	}
+    	mainStage.act(dt);
+    }
+    
+}
